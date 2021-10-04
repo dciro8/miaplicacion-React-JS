b044f2ec28473f6787da4d45ca47bacf46a8c214
David fernando Ciro Zaapta <dciro8@users.noreply.github.com>
2021-10-04 14:09:08 -0500

    Primer App in React JS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+// import logo from './logo.svg';
+
+// function HelloWord(props){
+//   return( 
+//   <div id="Hello">
+//     <h3>
+//     { props.subtitle}
+//     </h3>
+
+//     { props.myText} 
+//     </div>
+//     )
+// }
+
+class HelloWord extends React.Component{
+
+  
+  state = {
+    show:true
+   }
+
+   
+
+ToggleShow =  () => {
+  this.setState({show:!this.state.show})
+}
+
+  render(){
+
+    if (this.state.show)
+    {
+        
+    return  (  <div>            
+                  <div id="Hello">
+                    <h3>
+                      { this.props.subtitle}
+                    </h3>
+                      { this.props.myText} 
+                      <button onClick={ this.ToggleShow}>Change State</button>
+                  </div>
+                </div>)
+    }
+    else
+    {
+      return <div> <h3>Sin contenido
+           <button onClick={ this.ToggleShow}>Change State</button>
+      </h3>
+      </div>
+    }  
+  } 
+}
+
+
+// const App = () => <div>Este es el nuevo componente : <HelloWord/></div>
+
+// class App extends React.Component{
+//   render(){
+//     return <div>Este es el nuevo componenteVale : 
+//                 <HelloWord/> 
+//             </div>
+//   }
+// }
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+     <h1>
+       Este es el nuevo componente : 
+       <HelloWord 
+       myText="Hello Ciro" subtitle=" El mejor"/> 
+       <HelloWord 
+       myText="Hello Ciro1" subtitle=" El mejor1"/> 
+       <HelloWord 
+       myText="Hello Ciro2" subtitle=" El mejor2"/> 
+       </h1>      
+);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+// import React from 'react'; // importamos react
+// import ReactDOM from 'react-dom'; // nos permite renderizar en el DOM
+// const HolaMundo = <h1>Hola Mundo</h1>
+// //selecionamos el id donde se renderizara el componente
+// const root =  document.getElementById('root'); // hay un div con id root en index.html
+// ReactDOM.render(HolaMundo, root); // pinta en la pantalla la constante HolaMundo en el div con id root
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
